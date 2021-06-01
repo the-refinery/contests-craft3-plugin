5eaf39b705cba5e97873e29d7d0586053c13c80b
Brady <brady.bonnette@the-refinery.io>
2021-06-01 15:23:58 -0400

    Sets up Install.php migration
    This setup dupliates the original CraftCMS v2 table layout, but has been
    transformed to work with CraftCMS v3

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -72,18 +72,47 @@ class Install extends Migration
     {
         $tablesCreated = false;
 
-        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%craftcmscontests_craftcmscontestsrecord}}');
+        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%craftcms_contests}}');
         if ($tableSchema === null) {
             $tablesCreated = true;
             $this->createTable(
-                '{{%craftcmscontests_craftcmscontestsrecord}}',
+                '{{%craftcms_contests}}',
+                [
+                    'id' => $this->primaryKey(),
+                    'uid' => $this->uid(),
+                    'dateCreated' => $this->dateTime()->notNull(),
+                    'dateUpdated' => $this->dateTime()->notNull(),
+                    'name' => $this->string(255)->notNull(),
+                    'handle' => $this->string()->notNull(),
+                    'categories' => $this->text()->null(),
+                    'dateStart' => $this->dateTime()->null(),
+                    'dateEnd' => $this->dateTime()->null(),
+                    'lockoutLength' => $this->integer()->defaultValue(24)->null(),
+                    'lockoutFrequency' => $this->string(64)->defaultValue("hour")->null(),
+                    'enabled' => $this->boolean()->defaultValue(true)->null(),
+                    'sessionProtect' => $this->boolean()->defaultValue(true)->null(),
+                    'recaptchaSecret' => $this->text()->null(),
+                ]
+            );
+        }
+
+        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%craftcms_contests_votes}}');
+        if ($tableSchema === null) {
+            $tablesCreated = true;
+            $this->createTable(
+                '{{%craftcms_contests_votes}}',
                 [
                     'id' => $this->primaryKey(),
                     'dateCreated' => $this->dateTime()->notNull(),
                     'dateUpdated' => $this->dateTime()->notNull(),
                     'uid' => $this->uid(),
-                    'siteId' => $this->integer()->notNull(),
-                    'some_field' => $this->string(255)->notNull()->defaultValue(''),
+                    'contestId' => $this->integer()->null(),
+                    'categoryId' => $this->integer()->null(),
+                    'entryId' => $this->integer()->null(),
+                    'email' => $this->string(255)->null(),
+                    'ip' => $this->string(255)->null(),
+                    'extraData' => $this->text()->null(),
+                    'userAgent' => $this->string(512)->null(),
                 ]
             );
         }
@@ -96,23 +125,11 @@ class Install extends Migration
      */
     protected function createIndexes()
     {
-        $this->createIndex(
-            $this->db->getIndexName(
-                '{{%craftcmscontests_craftcmscontestsrecord}}',
-                'some_field',
-                true
-            ),
-            '{{%craftcmscontests_craftcmscontestsrecord}}',
-            'some_field',
-            true
-        );
-        // Additional commands depending on the db driver
-        switch ($this->driver) {
-            case DbConfig::DRIVER_MYSQL:
-                break;
-            case DbConfig::DRIVER_PGSQL:
-                break;
-        }
+        $this->createIndex(null, '{{%craftcms_contests}}', ['name'], true);
+        $this->createIndex(null, '{{%craftcms_contests}}', ['handle'], true);
+
+        $this->createIndex(null, '{{%craftcms_contests_votes}}', ['email'], true);
+        $this->createIndex(null, '{{%craftcms_contests_votes}}', ['email', 'categoryId'], true);
     }
 
     /**
@@ -120,15 +137,7 @@ class Install extends Migration
      */
     protected function addForeignKeys()
     {
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%craftcmscontests_craftcmscontestsrecord}}', 'siteId'),
-            '{{%craftcmscontests_craftcmscontestsrecord}}',
-            'siteId',
-            '{{%sites}}',
-            'id',
-            'CASCADE',
-            'CASCADE'
-        );
+        $this->addForeignKey(null, '{{%craftcms_contests_votes}}', ['contestId'], '{{%craftcms_contests}}', ['id'], 'CASCADE', null);
     }
 
     /**
@@ -143,6 +152,71 @@ class Install extends Migration
      */
     protected function removeTables()
     {
-        $this->dropTableIfExists('{{%craftcmscontests_craftcmscontestsrecord}}');
+        $this->dropTableIfExists('{{%craftcms_contests}}');
+        $this->dropTableIfExists('{{%craftcms_contests_votes}}');
     }
 }
+
+
+/*
+
+CraftCMS v2 DATA, keeping here for reference.
+mysql> describe craft_contestify_votes;
++-------------+--------------+------+-----+---------+----------------+
+| Field       | Type         | Null | Key | Default | Extra          |
++-------------+--------------+------+-----+---------+----------------+
+| id          | int(11)      | NO   | PRI | NULL    | auto_increment |
+| contestId   | int(10)      | YES  | MUL | NULL    |                |
+| categoryId  | int(10)      | YES  |     | NULL    |                |
+| entryId     | int(10)      | YES  |     | NULL    |                |
+| email       | varchar(255) | YES  | MUL | NULL    |                |
+| ip          | varchar(255) | YES  |     | NULL    |                |
+| extraData   | text         | YES  |     | NULL    |                |
+| userAgent   | varchar(512) | YES  |     | NULL    |                |
+| dateCreated | datetime     | NO   |     | NULL    |                |
+| dateUpdated | datetime     | NO   |     | NULL    |                |
+| uid         | char(36)     | NO   |     | 0       |                |
++-------------+--------------+------+-----+---------+----------------+
+
+mysql> show index from craft_contestify_votes;
++------------------------+------------+---------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| Table                  | Non_unique | Key_name                                    | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment |
++------------------------+------------+---------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| craft_contestify_votes |          0 | PRIMARY                                     |            1 | id          | A         |       31973 |     NULL | NULL   |      | BTREE      |         |               |
+| craft_contestify_votes |          1 | craft_contestify_votes_contestId_fk         |            1 | contestId   | A         |           2 |     NULL | NULL   | YES  | BTREE      |         |               |
+| craft_contestify_votes |          1 | craft_contestify_votes_email_idx            |            1 | email       | A         |       16487 |     NULL | NULL   | YES  | BTREE      |         |               |
+| craft_contestify_votes |          1 | craft_contestify_votes_email_categoryId_idx |            1 | email       | A         |       17028 |     NULL | NULL   | YES  | BTREE      |         |               |
+| craft_contestify_votes |          1 | craft_contestify_votes_email_categoryId_idx |            2 | categoryId  | A         |       16624 |     NULL | NULL   | YES  | BTREE      |         |               |
++------------------------+------------+---------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+
+mysql> describe craft_contestify_contests;
++------------------+--------------+------+-----+---------+----------------+
+| Field            | Type         | Null | Key | Default | Extra          |
++------------------+--------------+------+-----+---------+----------------+
+| id               | int(11)      | NO   | PRI | NULL    | auto_increment |
+| name             | varchar(255) | NO   | UNI | NULL    |                |
+| handle           | varchar(255) | NO   | UNI | NULL    |                |
+| categories       | text         | YES  |     | NULL    |                |
+| dateStart        | datetime     | YES  |     | NULL    |                |
+| dateEnd          | datetime     | YES  |     | NULL    |                |
+| lockoutLength    | int(11)      | YES  |     | 24      |                |
+| lockoutFrequency | varchar(64)  | YES  |     | hour    |                |
+| enabled          | tinyint(1)   | YES  |     | 1       |                |
+| sessionProtect   | tinyint(1)   | YES  |     | 1       |                |
+| recaptchaSecret  | text         | YES  |     | NULL    |                |
+| dateCreated      | datetime     | NO   |     | NULL    |                |
+| dateUpdated      | datetime     | NO   |     | NULL    |                |
+| uid              | char(36)     | NO   |     | 0       |                |
++------------------+--------------+------+-----+---------+----------------+
+
+mysql> show index from craft_contestify_contests;
++---------------------------+------------+------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| Table                     | Non_unique | Key_name                                 | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment |
++---------------------------+------------+------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| craft_contestify_contests |          0 | PRIMARY                                  |            1 | id          | A         |           2 |     NULL | NULL   |      | BTREE      |         |               |
+| craft_contestify_contests |          0 | craft_contestify_contests_id_unq_idx     |            1 | id          | A         |           2 |     NULL | NULL   |      | BTREE      |         |               |
+| craft_contestify_contests |          0 | craft_contestify_contests_name_unq_idx   |            1 | name        | A         |           2 |     NULL | NULL   |      | BTREE      |         |               |
+| craft_contestify_contests |          0 | craft_contestify_contests_handle_unq_idx |            1 | handle      | A         |           2 |     NULL | NULL   |      | BTREE      |         |               |
++---------------------------+------------+------------------------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+*/
+
